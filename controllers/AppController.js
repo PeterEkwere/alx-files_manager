49e8d82cd55a96a9d330b85f4544aef83855c572
PeterEkwere <peterekwere@icloud.com>
2024-03-13 16:52:15 +0100

    update 2 for task 2

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -2,26 +2,16 @@ import dbClient from '../utils/db';
 import redisClient from '../utils/redis';
 
 class AppController {
-  static async getStatus(req, res) {
-    try {
-      const redisLive = redisClient.isAlive();
-      const dbLive = dbClient.isAlive();
-      res.status(200).json({ redis: redisLive, db: dbLive });
-    } catch (error) {
-      console.error('Error checking status:', error);
-      res.status(500).json({ error: 'Internal server error' });
-    }
+  static getStatus(req, res) {
+    const redisLive = redisClient.isAlive();
+    const dbLive = dbClient.isAlive();
+    res.status(200).json({ redis: redisLive, db: dbLive });
   }
 
   static async getStats(req, res) {
-    try {
-      const usersTotal = await dbClient.nbUsers();
-      const filesTotal = await dbClient.nbFiles();
-      res.status(200).json({ users: usersTotal, files: filesTotal });
-    } catch (error) {
-      console.error('Error getting stats:', error);
-      res.status(500).json({ error: 'Internal server error' });
-    }
+    const usersTotal = await dbClient.nbUsers();
+    const filesTotal = await dbClient.nbFiles();
+    res.status(200).json({ users: usersTotal, files: filesTotal });
   }
 }
 
--- server.js
+++ server.js
@@ -3,11 +3,8 @@ import routes from './routes/index';
 
 const app = express();
 app.use(express.json());
-
-// Use environment variable for port or default to 5000
 const port = process.env.PORT || 5000;
 
-// Mount all routes from index.js
 app.use('/', routes);
 
 app.listen(port, () => {
